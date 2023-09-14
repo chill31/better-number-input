93628e2be7ec1d00a11e73502958d7b65473eede
chill31 <89910163+chill31@users.noreply.github.com>
2023-09-14 14:44:24 +0530

    updated functions and added options.

--- README.md
+++ README.md
@@ -4,4 +4,88 @@ Use it in your html:
 ```js
 <script src="https://cdn.jsdelivr.net/gh/chill31/better-number-input/better-number-input.js"></script>
 ```
-at the end of the body tag, so it selects all number inputs and implements strict number only behaviour.
+at the end of the body tag **BUT** before all scripts, so it selects all number inputs and implements strict number only behaviour.
+<br>
+<br>
+
+## Brief
+this file does simple things. First, it removes the annoying "up" and "down" arrows in the number inputs. This is done by just converting the `type="number"` attribute to `type="text"`.
+
+> This is not a problem. For accessibility, the input will still be rendered as a "number" input for those using assistive technologies.
+<br>
+<br>
+
+## Options
+There are a few options you can customize to change the behaviour of the "number" input.<br>
+The default are as follows:
+```js
+{
+  allowNegative: true,
+  allowDecimal: true,
+  allowScientificNotation: false,
+  valueAsNumber: 0,
+  resetValues: true
+}
+```
+These options are set through javascript through `<input>.betterInputOptions`.<br>
+To customize these, you can do:
+```js
+const myInput = document.querySelector('.my-number-input');
+
+myInput.betterInputOptions = {
+  allowNegative: false,
+  allowDecimal: false,
+  allowScientificNotation: true,
+  valueAsNumber: 1, // won't actually change the input value.
+  resetValues: false
+}
+```
+<br>
+<br>
+
+## Detailed Options
+<br>
+<br>
+
+### `allowNegative`
+this option is a boolean type. (`true, false`) <br>
+This allows you to set whether negatives will be allowed or not. (kind of obvious)
+<br>
+<br>
+
+### `allowDecimal`
+this option is a boolean type (`true, false`) <br>
+This allows you to set whether a decimal point (.) will be allowed to enter or not.
+<br>
+<br>
+
+### `allowScientificNotation`
+this option is a boolean type (`true, false`) <br>
+This allows you to set whether scientific notation will be allowed or not.<br>
+For those of you who don't know, this is how scientific notation is used:
+
+```js
+console.log(3e4); // valid Javascript syntax (scientific notation)
+
+// OUTPUT: 30000
+```
+<br>
+<br>
+
+### `valueAsNumber`
+this value is supposed to be read-only. It is a type of `number`. As you read in [the beginning](#brief), the number input is changed to "text" input for certain features and removing the "up down" arrows.<br>
+In the number inputs, there was a parameter of `valueAsNumber` to give the value of the input as number type, but this won't be possible with text inputs.
+<br>
+To get the value as number in the "better" number inputs, use
+
+```js
+const myInput = document.querySelector('.my-number-input');
+console.log(myInput.betterInputOptions.valueAsNumber);
+```
+<br>
+<br>
+
+### `resetValues`
+Input values are saved after refreshes for some time, so this option allows you to reset the value of the inputs each time the browser tab refreshes.<br>
+This option is a boolean type (`true, false`)
+<br>
--- better-number-input.js
+++ better-number-input.js
@@ -1,12 +1,46 @@
 // selecting all the inputs which are number type and not disabled through attribute.
-const allInputs = document.querySelectorAll("input[type=\"number\"]:not([disable-better-number-input])");
+const allNormalNumberInputs = document.querySelectorAll("input[type=\"number\"]:not([disable-better-number-input])");
+
+// example options for the "better" number input
+const exampleOptionsForBetterNumberInputs = {
+  allowNegative: true,
+  allowDecimal: true,
+  allowScientificNotation: false,
+  valueAsNumber: 0,
+  resetValues: true
+}
 
 // adding event listeners to all inputs
-allInputs.forEach((input) => {
+allNormalNumberInputs.forEach((input) => {
+
+  input.setAttribute("type", "text");
+  input.setAttribute("aria-type", "number");
+
+  if (!input.betterInputOptions || typeof input.betterInputOptions !== "object") {
+    input.betterInputOptions = exampleOptionsForBetterNumberInputs;
+  }
+
+  if (typeof input.betterInputOptions.allowNegative !== typeof true) {
+    input.betterInputOptions.allowNegative = exampleOptionsForBetterNumberInputs.allowNegative;
+  }
+
+  if (typeof input.betterInputOptions.allowDecimal !== typeof true) {
+    input.betterInputOptions.allowDecimal = exampleOptionsForBetterNumberInputs.allowDecimal;
+  }
+
+  if (typeof input.betterInputOptions.allowScientificNotation !== typeof true) {
+    input.betterInputOptions.allowScientificNotation = exampleOptionsForBetterNumberInputs.allowScientificNotation;
+  }
+
+  if(typeof input.betterInputOptions.resetValues !== typeof true) {
+    input.betterInputOptions.resetValues = exampleOptionsForBetterNumberInputs.resetValues;
+  }
+
+  if(input.betterInputOptions.resetValues) {
+    input.value = "";
+  }
 
   input.addEventListener("keypress", (e) => {
-
-    console.log(e.key);
     const firstDotIndex = e.target.value.indexOf(".");
 
     const hasOnePeriod = firstDotIndex !== -1 && e.target.value.indexOf(".", firstDotIndex + 1) === -1;
@@ -19,10 +53,62 @@ allInputs.forEach((input) => {
 
       if (e.key === '.') {
 
+        if (input.betterInputOptions.allowDecimal === false) {
+          e.preventDefault();
+        } else {
+
+          if (e.target.value.indexOf('.') !== -1) {
+            e.preventDefault();
+          } else {
+
+            if (e.target.value.split("").includes('-') && e.target.value.split("").indexOf('-') !== 0) {
+              e.preventDefault();
+            } else {
+
+            }
+
+          }
+
+        }
+
+      } else if (e.key === 'e') {
+
+        if (input.betterInputOptions.allowScientificNotation === false) {
+          e.preventDefault();
+        } else {
+
+          if (e.target.value.indexOf('e') !== -1) {
+            e.preventDefault();
+          } else {
+            if (e.target.value.length === 0) {
+              e.preventDefault();
+            }
+          }
+
+        }
+
+      } else if (e.key === '-') {
+
+        if (input.betterInputOptions.allowNegative === false) {
+          e.preventDefault();
+        } else {
+
+          if (e.target.value.indexOf('-') !== -1) {
+            e.preventDefault();
+          } else {
+            if (e.target.selectionStart !== 0) {
+              e.preventDefault();
+            }
+          }
+
+        }
+
       } else {
+
         if (/\d/.test(Number(e.key)) === false) {
           e.preventDefault();
         }
+
       }
 
     }
@@ -30,14 +116,11 @@ allInputs.forEach((input) => {
   });
 
   input.addEventListener("paste", (e) => {
-    console.log(e.clipboardData.getData("text/plain"));
-    if (e.target.value.indexOf(".") !== -1 && e.clipboardData.getData("text/plain").indexOf(".") !== -1) {
-      e.preventDefault();
-    }
-
     if (isNaN(e.clipboardData.getData("text/plain"))) {
       e.preventDefault();
     }
   });
+
+  input.betterInputOptions.valueAsNumber = Number(input.value);
 
 });
